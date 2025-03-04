e579e0090ccb7d7e3e5b705907cec1e542ad5518
Prakashgupta41532 <pgupta41532@gmail.com>
2025-03-05 00:17:09 +0530

    minorFixes

--- src/app/afterleadingpage/page.js
+++ src/app/afterleadingpage/page.js
@@ -183,10 +183,13 @@ export const AfterLeadingPage = () => {
           </Card>
         ))}
       </div> */}
-      <div className="flex flex-row gap-5 p-4 md:p-8">
-        <ProductCardFirst />
-        <ProductCardSecond />
-        <ProductCardThird />
+      {/* <div className="flex flex-row gap-5 p-4 md:p-8"> */}
+      <div className="max-w-7xl mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5 p-4 md:p-8 justify-items-center">
+          <ProductCardFirst />
+          <ProductCardSecond />
+          <ProductCardThird />
+        </div>
       </div>
 
       <div className="bg-gray-100 text-black p-6 md:p-10 mt-10 rounded-lg">
--- src/components/Header.js
+++ src/components/Header.js
@@ -127,7 +127,29 @@ const Header = () => {
             </a>
           </Dropdown>
         )}
-
+        {userSession && (
+          <div className="flex items-center cursor-pointer relative">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="text-[#00237D]"
+            >
+              <circle cx="9" cy="21" r="1"></circle>
+              <circle cx="20" cy="21" r="1"></circle>
+              <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+            </svg>
+            <div className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+              3
+            </div>
+          </div>
+        )}
         <NavbarMenu>
           {menuItems.map((item, index) => (
             <NavbarMenuItem key={item.route}>
